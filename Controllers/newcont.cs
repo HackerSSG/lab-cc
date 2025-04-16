d847f18f98ccb4073401639f5d4896909c0780eb
spring2018 <spring2018@SELAB8-019>
2025-04-16 15:34:05 +0500

    test

---
+++ Controllers/newcont.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab_cc.Controllers
+{
+    public class newcont
+    {
+    }
+}
---
+++ Models/student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab_cc.Models
+{
+    public class student
+    {
+
+        public int id { get ; set; }
+
+        public string name { get; set;}
+
+        public string description { get; set;}
+
+
+        public int age { get; set;}
+    }
+}
